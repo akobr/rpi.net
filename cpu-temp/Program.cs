005ad79522697f7d5a9cf0db547737fe626b4d6c
Ales Kobr <ales.kobr@bricknode.com>
2021-12-29 20:08:48 +0100

    init of code and solution

--- cpu-temp/Program.cs
+++ cpu-temp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Iot.Device.CpuTemperature;
+
+using var cpuTemp = new CpuTemperature();
+Console.WriteLine("Press any key to quit.");
+
+while (!Console.KeyAvailable)
+{
+    if (!cpuTemp.IsAvailable)
+    {
+        Console.WriteLine("CPU temperature is not available. :(");
+        Thread.Sleep(5000);
+        continue;
+    }
+
+    var temps = cpuTemp.ReadTemperatures();
+    foreach (var entry in temps)
+    {
+        Console.WriteLine(!double.IsNaN(entry.Temperature.DegreesCelsius)
+            ? $"Temperature from {entry.Sensor} is {Math.Round(entry.Temperature.DegreesCelsius, 2)} °C."
+            : $"Unable to read temperature from {entry.Sensor}. :(");
+    }
+    Thread.Sleep(2500);
+}
--- led-stripe/Program.cs
+++ led-stripe/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Device.Spi;
-using System.Device.Spi.Drivers;
+﻿using System.Device.Spi;
 using System.Drawing;
-using IoT.Device.Graphics;
-using IoT.Device.Ws28xx; 
+using Iot.Device.Ws28xx;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
@@ -17,14 +14,13 @@ var settings = new SpiConnectionSettings(0,0)
     DataBitLength = 8,
 };
 
-
-var spi = new UnixSpiDevice(settings);
+var spi = SpiDevice.Create(settings);
 var device = new Ws2812b(spi, count);
 
-BitmapImage image = device.Image;
+var image = device.Image;
 image.Clear();
 
-for(int i = 0; i < count; i++)
+for(var i = 0; i < count; i++)
 {
     image.SetPixel(i, 0, Color.Green);
 }
